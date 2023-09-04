f99c01315c5807571f56c15a5b780b5b93e2ba98
afoadomain <soufianmejji@gmail.com>
2023-09-04 02:54:36 +0100

    ads.txt

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2901138165202171"
+     crossorigin="anonymous"></script>
   return (
     <Fragment>
       <DefaultSeo
@@ -16,6 +18,7 @@ function MyApp({ Component, pageProps }) {
           site_name: "Youtube Thumbnail Downloader",
         }}
       />
+
       <Component {...pageProps} />
     </Fragment>
   );
