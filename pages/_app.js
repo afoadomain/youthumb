14b3c99423005bb25cb33882037def55b74a6011
afoadomain <soufianmejji@gmail.com>
2023-09-05 13:36:26 +0100

    edit

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,70 @@ import { Fragment } from "react";
 import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2901138165202171"
-     crossorigin="anonymous"></script>
   return (
     <Fragment>
+      
+
       <DefaultSeo
-        title="Youtube Thumbnail Downloader"
-        description="Download high-quality thumbnails from YouTube videos."
-        canonical="https://your-website-url.com"
+        title="YouTube Thumbnail Downloader"
+        description="Save high-quality YouTube video thumbnails in various resolutions, including Full HD (1080p), HD (720p), and SD. Our YouTube Thumbnail Grabber supports multiple video formats, including HD, HQ, 1080p, and 4K videos. Learn how to use our YouTube Thumbnail Downloader to extract thumbnails from any video and ensure you respect copyright rules."
+        canonical="https://wwww.feedka.com"
         openGraph={{
-          url: "https://your-website-url.com",
-          title: "Youtube Thumbnail Downloader",
-          description: "Download high-quality thumbnails from YouTube videos.",
-          site_name: "Youtube Thumbnail Downloader",
+          url: "https://wwww.feedka.com",
+          title: "YouTube Thumbnail Downloader",
+          description: "Save high-quality YouTube video thumbnails in various resolutions, including Full HD (1080p), HD (720p), and SD. Our YouTube Thumbnail Grabber supports multiple video formats, including HD, HQ, 1080p, and 4K videos. Learn how to use our YouTube Thumbnail Downloader to extract thumbnails from any video and ensure you respect copyright rules.",
+          site_name: "YouTube Thumbnail Downloader",
         }}
       />
+{/* YouTube Thumbnail Downloader - Save YT Video Thumbnails
+Get high-quality free thumbnail images from any YouTube video in various resolutions, including Full HD (1080p), HD (720p), and SD. Our YouTube Thumbnail Grabber supports multiple video formats, including HD, HQ, 1080p, and 4K videos.
 
+Why Use Our YouTube Thumbnail Grabber Website?
+Our YouTube Thumbnail Downloader is a valuable tool for extracting thumbnails from any YouTube video. These thumbnails can be used in presentations, animation projects, and various other creative activities.
+
+How to Use Our YouTube Thumbnail Downloader?
+1. Copy the URL of the YouTube video whose thumbnail you want to download.
+2. Paste the URL into the input box on our website.
+3. Our tool will instantly generate thumbnails in different sizes.
+4. Click the download button to save the thumbnail to your device.
+
+Please note: While downloading YouTube thumbnails is legal, both thumbnails and videos are copyrighted materials. Be sure to obtain proper permissions from the content creator before reusing them.
+
+Compatibility of Our YT Thumbnail Grabber Website
+Our YouTube Thumbnail Downloader is compatible with most devices, including Android phones and desktop systems. However, it may not work on iPhones due to restrictions on image downloads. Jailbroken iPhones may not encounter this issue.
+
+Is There a Copyright Risk with YouTube Thumbnails?
+Downloading YouTube thumbnails may involve copyright considerations. Always respect the copyrights of video owners and seek permission if you plan to use the thumbnails, especially within the YouTube platform.
+
+Are Reused YouTube Thumbnails SEO Friendly?
+Reusing YouTube thumbnails is generally not considered SEO friendly because Google indexes most YouTube thumbnails. To enhance SEO friendliness, consider adding unique effects using software like Photoshop to make your thumbnail stand out and appear unique to search engines.
+
+Download YouTube video thumbnails hassle-free with our YouTube Thumbnail Downloader.*/}
+<p>Welcome to our website! We provide high-quality YouTube thumbnail downloads in various resolutions. Whether you need Full HD, HD, or SD thumbnails, we've got you covered.</p>
+  {/* YouTube Thumbnail Downloader - Save YT Video Thumbnails
+Get high-quality free thumbnail images from any YouTube video in various resolutions, including Full HD (1080p), HD (720p), and SD. Our YouTube Thumbnail Grabber supports multiple video formats, including HD, HQ, 1080p, and 4K videos.
+
+Why Use Our YouTube Thumbnail Grabber Website?
+Our YouTube Thumbnail Downloader is a valuable tool for extracting thumbnails from any YouTube video. These thumbnails can be used in presentations, animation projects, and various other creative activities.
+
+How to Use Our YouTube Thumbnail Downloader?
+1. Copy the URL of the YouTube video whose thumbnail you want to download.
+2. Paste the URL into the input box on our website.
+3. Our tool will instantly generate thumbnails in different sizes.
+4. Click the download button to save the thumbnail to your device.
+
+Please note: While downloading YouTube thumbnails is legal, both thumbnails and videos are copyrighted materials. Be sure to obtain proper permissions from the content creator before reusing them.
+
+Compatibility of Our YT Thumbnail Grabber Website
+Our YouTube Thumbnail Downloader is compatible with most devices, including Android phones and desktop systems. However, it may not work on iPhones due to restrictions on image downloads. Jailbroken iPhones may not encounter this issue.
+
+Is There a Copyright Risk with YouTube Thumbnails?
+Downloading YouTube thumbnails may involve copyright considerations. Always respect the copyrights of video owners and seek permission if you plan to use the thumbnails, especially within the YouTube platform.
+
+Are Reused YouTube Thumbnails SEO Friendly?
+Reusing YouTube thumbnails is generally not considered SEO friendly because Google indexes most YouTube thumbnails. To enhance SEO friendliness, consider adding unique effects using software like Photoshop to make your thumbnail stand out and appear unique to search engines.
+
+Download YouTube video thumbnails hassle-free with our YouTube Thumbnail Downloader. */}
       <Component {...pageProps} />
     </Fragment>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,23 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" /> {/* This is the correct comment for adding a description */}
+          <meta
+            name="description"
+            content="Elevate your YouTube channel to new heights with our effortless solution for accessing high-quality thumbnails in a range of resolutions. Our user-friendly application not only simplifies the process of thumbnail downloads but also offers you the opportunity to draw inspiration from the creative works of fellow content creators. It's the perfect tool to enhance and invigorate your own video content.
+
+            With just a simple action, you can transform the visual appeal of your YouTube videos. Paste the video URL into our intuitive interface, and with a quick click on the 'Download' button, you'll have high-quality thumbnails ready to captivate your audience.
+            
+            Say goodbye to the hassle of searching for the right thumbnail – our application provides a seamless experience, ensuring your content stands out in style.
+            
+            Take the first step towards making your YouTube channel more engaging and memorable today."
+          /> {/* Elevate your YouTube channel to new heights with our effortless solution for accessing high-quality thumbnails in a range of resolutions. Our user-friendly application not only simplifies the process of thumbnail downloads but also offers you the opportunity to draw inspiration from the creative works of fellow content creators. It's the perfect tool to enhance and invigorate your own video content.
+
+          With just a simple action, you can transform the visual appeal of your YouTube videos. Paste the video URL into our intuitive interface, and with a quick click on the 'Download' button, you'll have high-quality thumbnails ready to captivate your audience.
+          
+          Say goodbye to the hassle of searching for the right thumbnail – our application provides a seamless experience, ensuring your content stands out in style.
+          
+          Take the first step towards making your YouTube channel more engaging and memorable today. */}
           <link
             rel="apple-touch-icon"
             sizes="180x180"
@@ -23,7 +40,7 @@ class MyDocument extends Document {
             href="/favicon/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <script data-ad-client="YOUR_ADSENSE_CLIENT_ID" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          
         </Head>
         <body>
           <Main />
