b40f769b25e298c190c23cf43b6441c712f98a9e
afoadomain <soufianmejji@gmail.com>
2023-09-05 03:12:00 +0100

    letle

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ const Index = () => {
   return (
     <div className="container mx-auto px-4 py-8">
       <header className="text-center mb-8" style={{ width: '62%' }}>
-        <h1 className="text-3xl font-bold mb-2">
+      <h1 className="text-3xl font-bold mb-2">
           Free Youtube Thumbnail Downloader
         </h1>
         <p className="text-gray-600" style={{ width: '100%' }}>
